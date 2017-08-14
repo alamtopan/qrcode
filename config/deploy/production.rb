03c81e283a1818a0f0375c7b9495eeb4a6ec297d
mac <alamtopani@yahoo.com>
2017-08-14 10:58:10 +0700

    revision dep

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ set :stage, :production
 server '63.142.253.124', user: 'deploy', roles: %w{web app}
 
 set :rbenv_type,     :system
-set :rbenv_ruby,     '2.3.0'
+set :rbenv_ruby,     '2.3.1'
 set :rbenv_prefix,   "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles,    :all
